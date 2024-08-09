416cc1087b797370227c4b03eb6314ef38a97f15
kvardaan <vardaankhattar8907@gmail.com>
2024-08-09 12:37:12 +0530

    Project Initialized & Setup

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["./src/**/*.tsx"],
-  prefix: "ui-",
-  presets: [sharedConfig],
+	content: ["./src/**/*.tsx"],
+	prefix: "",
+	presets: [sharedConfig],
 };
 
 export default config;
